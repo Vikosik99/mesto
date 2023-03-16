1e346c2330dc2013bffceea7fa7fe508fa415e6a
Viktoria Belkova <viktoria.b99@yandex.ru>
2023-03-16 14:05:25 +0300

    fix: Исправлены основные ошибки в html и все в JS

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-let closeButton = document.querySelector('.close-button');
-let editButton = document.querySelector('.edit-button');
+// Задаем переменные
+
+let popupButtonClose = document.querySelector('.popup__button-close');
+let profileButtonEdit = document.querySelector('.profile__button-edit');
 let popup = document.querySelector('.popup');
-let formUsername = document.querySelector('.form__username');
-let formStatus = document.querySelector('.form__status')
+let formInput = document.querySelector('.form__input');
+let formInputKyeUsername = document.querySelector('.form__input_kye_username');
+let formInputKyeStatus = document.querySelector('.form__input_kye_status')
 let profileUsername = document.querySelector('.profile__username')
 let profileStatus = document.querySelector('.profile__status');
+let formButtonSave = document.querySelector('.form__button-save');
 
-
-
-editButton.addEventListener('click', function(){
-    popup.classList.add('popup_opened');
-})
-
-
-closeButton.addEventListener('click', function(){
-    popup.classList.remove('popup_opened');
-})
+// Обьявляем функции
 
 function popupClick() {
-    formUsername.value = profileUsername.textContent;
-    formStatus.value = profileStatus.textContent;
+    popup.classList.add('popup_opened');
+    formInputKyeUsername.value = profileUsername.textContent;  //реализуем перенос текста из профиля в попап
+    formInputKyeStatus.value = profileStatus.textContent;
 }
 
-editButton.addEventListener('click', popupClick);
-
-
-document.querySelector('.save-button').onclick = saveClick;
-
-function saveClick() {
-    let formUsername = document.querySelector('.form__username').value;
-    document.querySelector('.profile__username').innerHTML = formUsername;
-
-    let formStatus = document.querySelector('.form__status').value;
-   document.querySelector('.profile__status').innerHTML = formStatus;
-
-   popup.classList.remove('popup_opened');
+function popupClose() {
+    popup.classList.toggle('popup_opened'); 
 }
+
+// Реализуем открытие и закрытие попапа
+
+profileButtonEdit.addEventListener('click', popupClick); 
+popupButtonClose.addEventListener('click', popupClose)
+
+// Реализуем отправку изменений из попапа в профиль
+
+function saveClick(event) {
+    event.preventDefault();
+
+    profileUsername.textContent = formInputKyeUsername.value;
+    profileStatus.textContent = formInputKyeStatus.value;
+
+    popup.classList.remove('popup_opened'); 
+}
+popup.addEventListener('submit', saveClick);
