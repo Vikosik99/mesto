7ffede07c1e5d11efbeffa51b533cf84469d87dc
Viktoria Belkova <viktoria.b99@yandex.ru>
2023-03-17 19:58:20 +0300

    fix:Исправлена верства html и js

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,11 @@
 let popupButtonClose = document.querySelector('.popup__button-close');
 let profileButtonEdit = document.querySelector('.profile__button-edit');
 let popup = document.querySelector('.popup');
-let formInput = document.querySelector('.form__input');
 let formInputKyeUsername = document.querySelector('.form__input_kye_username');
 let formInputKyeStatus = document.querySelector('.form__input_kye_status')
 let profileUsername = document.querySelector('.profile__username')
 let profileStatus = document.querySelector('.profile__status');
-let formButtonSave = document.querySelector('.form__button-save');
+let form = document.querySelector('.form');
 
 // Обьявляем функции
 
@@ -22,11 +21,6 @@ function popupClose() {
     popup.classList.toggle('popup_opened'); 
 }
 
-// Реализуем открытие и закрытие попапа
-
-profileButtonEdit.addEventListener('click', popupClick); 
-popupButtonClose.addEventListener('click', popupClose)
-
 // Реализуем отправку изменений из попапа в профиль
 
 function saveClick(event) {
@@ -35,6 +29,14 @@ function saveClick(event) {
     profileUsername.textContent = formInputKyeUsername.value;
     profileStatus.textContent = formInputKyeStatus.value;
 
-    popup.classList.remove('popup_opened'); 
+    popupClose();
 }
-popup.addEventListener('submit', saveClick);
+
+// Реализуем открытие и закрытие попапа
+
+profileButtonEdit.addEventListener('click', popupClick); 
+popupButtonClose.addEventListener('click', popupClose)
+
+// Отправка формы
+
+form.addEventListener('submit', saveClick);
